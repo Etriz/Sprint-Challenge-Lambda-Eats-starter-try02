d74498a9194b4ea67ed506b9d05b45ca21de13a4
ryanp <ryanpanimation@gmail.com>
2020-05-02 12:45:11 -0500

    error handling matching schema

--- src/components/Form.js
+++ src/components/Form.js
@@ -6,8 +6,9 @@ const Form = () => {
   const [formState, setFormstate] = useState({
     name: "",
     size: "",
-    sauce: "original",
-    pepperoni: true,
+    sauce: "",
+    cheese: true,
+    pepperoni: false,
     sausage: false,
     bacon: false,
     mushrooms: false,
@@ -16,24 +17,35 @@ const Form = () => {
   const [errorState, setErrorstate] = useState({
     name: "",
     size: "",
-    sauce: "",
+    sauce: "Must choose a sauce",
     pepperoni: "",
     sausage: "",
     bacon: "",
     mushrooms: "",
     special: "",
   });
+  const [isButtonDisabled, setIsButtonDisabled] = useState(true);
+  //!end states
   const formSchema = yup.object().shape({
     name: yup.string().min(2).required("Name is required"),
-    size: yup.string().required("Size is required"),
-    sauce: yup.string().required("Sauce is required"),
+    size: yup.mixed().oneOf(["personal", "medium", "large"]).required("Must choose a size"),
+    sauce: yup.mixed().oneOf(["original", "alfredo"]).required("Must choose a sauce"),
+    cheese: yup.boolean(),
     pepperoni: yup.boolean(),
     sausage: yup.boolean(),
     bacon: yup.boolean(),
     mushrooms: yup.boolean(),
-    instructions: yup.string(),
+    special: yup.string().notRequired(),
   });
-  const handleChange = (e) => {};
+  const handleChange = (e) => {
+    e.persist();
+    const newFormData = {
+      ...formState,
+      [e.target.name]: e.target.type === "checkbox" ? e.target.checked : e.target.value,
+    };
+    validateChange(e);
+    setFormstate(newFormData);
+  };
   const validateChange = (e) => {
     yup
       .reach(formSchema, e.target.name)
@@ -45,65 +57,124 @@ const Form = () => {
         setErrorstate({ ...errorState, [e.target.name]: err.errors[0] });
       });
   };
-  const submitForm = (e) => {
+  const formSubmit = (e) => {
     e.preventDefault();
     console.log("Form Submitted");
   };
+
+  useEffect(() => {
+    console.log(formState);
+  }, [formState]);
+  useEffect(() => {
+    formSchema.isValid(formState).then((valid) => {
+      console.log("is it valid?", valid);
+      return valid ? setIsButtonDisabled(false) : null;
+    });
+  }, [formState]);
+
   return (
-    <form onSubmit={submitForm}>
+    <form onSubmit={formSubmit}>
       <div>
         <label htmlFor="name">
           Name
-          <input type="text" id="name" name="name" placeholder="name" />
+          <input type="text" id="name" name="name" placeholder="name" onChange={handleChange} />
         </label>
+        {errorState.name.length > 0 ? <p>{errorState.name}</p> : null}
       </div>
       <div>
         <label htmlFor="size">
           Size
-          <select id="size" name="size">
+          <select id="size" name="size" onChange={handleChange}>
             <option value="">--Choose--</option>
             <option value="personal">Personal</option>
             <option value="medium">Medium</option>
             <option value="large">Large</option>
           </select>
         </label>
+        {errorState.size.length > 0 ? <p>{errorState.size}</p> : null}
       </div>
       <div>
         <label htmlFor="original">
-          <input type="radio" name="sauce" id="original" value="original" checked />
+          <input type="radio" name="sauce" id="original" value="original" onChange={handleChange} />
           original
         </label>
         <label htmlFor="alfredo">
-          <input type="radio" name="sauce" id="alfredo" value="alfredo" />
+          <input type="radio" name="sauce" id="alfredo" value="alfredo" onChange={handleChange} />
           alfredo
         </label>
+        {errorState.sauce.value !== "" ? (
+          <>
+            <br />
+            <span> {errorState.sauce}</span>
+          </>
+        ) : null}
       </div>
       <div>
+        <label htmlFor="cheese">
+          <input
+            type="checkbox"
+            id="cheese"
+            name="cheese"
+            checked={formState.cheese}
+            onChange={handleChange}
+          />
+          cheese
+        </label>
         <label htmlFor="pepperoni">
-          <input type="checkbox" id="pepperoni" name="pepperoni" />
+          <input
+            type="checkbox"
+            id="pepperoni"
+            name="pepperoni"
+            checked={formState.pepperoni}
+            onChange={handleChange}
+          />
           pepperoni
         </label>
         <label htmlFor="sausage">
-          <input type="checkbox" id="sausage" name="sausage" />
+          <input
+            type="checkbox"
+            id="sausage"
+            name="sausage"
+            checked={formState.sausage}
+            onChange={handleChange}
+          />
           sausage
         </label>
         <label htmlFor="bacon">
-          <input type="checkbox" id="bacon" name="bacon" />
+          <input
+            type="checkbox"
+            id="bacon"
+            name="bacon"
+            checked={formState.bacon}
+            onChange={handleChange}
+          />
           bacon
         </label>
         <label htmlFor="mushrooms">
-          <input type="checkbox" id="mushrooms" name="mushrooms" />
+          <input
+            type="checkbox"
+            id="mushrooms"
+            name="mushrooms"
+            checked={formState.mushrooms}
+            onChange={handleChange}
+          />
           mushrooms
         </label>
       </div>
       <div>
         <label htmlFor="special">
           special instructions
-          <input type="text" id="special" name="special" placeholder="special instructions" />
+          <input
+            type="text"
+            id="special"
+            name="special"
+            placeholder="special instructions"
+            onChange={handleChange}
+          />
         </label>
       </div>
       <div>
-        <button>Order Now!</button>
+        <button disabled={isButtonDisabled}>Order Now!</button>
       </div>
     </form>
   );
